d21b0d5a51ad9b602f4f2b6debed5b08d802a019
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-02-13 17:29:06 +0000

    ok

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "my"];
+  let adjectives = ["favorite", "fabolous"];
+  let nouns = ["project", "code"];
+  let extension = [".com", ".es"];
+
+  const domainGenerator = () => {
+    let domainNames = [];
+
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjectives.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          for (let m = 0; m < extension.length; m++) {
+            let domainName;
+
+            let extensionMatch = extension[m].replace(".", "");
+            if (nouns[k].endsWith(extensionMatch)) {
+              let modifiedNoun = nouns[k].slice(0, -extensionMatch.length);
+              domainName =
+                pronouns[i] + adjectives[j] + modifiedNoun + extension[m];
+            } else {
+              domainName =
+                pronouns[i] + adjectives[j] + nouns[k] + extension[m];
+            }
+
+            domainNames.push(domainName);
+          }
+        }
+      }
+    }
+    return domainNames;
+  };
+  let result = domainGenerator();
+
+  result.forEach((value, i) => {
+    console.log(value, i);
+  });
 };
